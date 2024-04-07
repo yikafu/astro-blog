0555ee6267446f293b5baa7414f3d9c3d66889ac
yikafu <f2149804524@gmail.com>
2024-04-07 15:39:51 +0800

    commit

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from "astro:content";
+
+const postsCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    date: z.date(),
+    tags: z.array(z.string()).optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+export const collections = {
+  posts: postsCollection,
+};
