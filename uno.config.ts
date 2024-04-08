73a6190e4e58fabe07045ded9842460fe9e0523b
yikafu <f2149804524@gmail.com>
2024-04-08 20:51:11 +0800

    unocss调整

--- uno.config.ts
+++ uno.config.ts
@@ -6,38 +6,25 @@ import transformerDirectives from "@unocss/transformer-directives";
 export default defineConfig({
   presets: [presetAttributify(), presetUno({ dark: "class" })],
   transformers: [transformerCompileClass(), transformerDirectives()],
-  rules: [
-    [
-      "rules-flex",
-      {
-        display: "flex",
-        "justify-content": "center",
-        "align-items": "center",
-      },
-    ],
-    [
-      "rules-b-base",
-      {
-        border: "1px solid #666",
-      },
-    ],
-    ["rules-ts", { transition: "all 0.3s ease" }],
-    [
-      "rules-ly",
-      { "--at-apply": "h-100% p-8 b-rd-4 bg-card dark:bg-dark-card" },
-    ],
-  ],
+  shortcuts: {
+    "sc-flex": "flex justify-center items-center",
+    "sc-ts": "transition-all duration-300 ease",
+    "sc-ly": "h-100% p-8 b-rd-4 bg-card dark:bg-dark-card",
+    "sc-base": "border b-solid b-#666",
+    "sc-card": "bg-card dark:bg-dark-card",
+    "sc-bg": "bg-bgcolor dark:bg-dark-bgcolor",
+    "sc-hover": "bg-second text-primary hover:bg-primary hover:text-second",
+  },
   theme: {
     colors: {
       card: "#f5f5f5",
       bgcolor: "#dae1e7",
       primary: "#326ca3",
       second: "#d6e2ed",
-      "dark-bgcolor": "#282c34",
-      "dark-card": "#354e6b",
-    },
-    breakpoints: {
-
+      dark: {
+        card: "#354e6b",
+        bgcolor: "#282c34",
+      },
     },
   },
 });
